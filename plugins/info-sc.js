38f99630e6d5dcc63c9f5b239863e6f8d01fbbf4
Nazir⚡ <nazir99iq@gmail.com>
2024-12-16 13:20:49 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,8 +1,17 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
-`
+Hai ${ye} 
+\n– 乂 I N F O - S C R I P T*
+\n◦  Name : \n◦  Modules Type  : CjsxEsm \n◦  Format : Plugins\n◦  Total Features : 600+ \n◦  Price : 65.000  \n◦  Review Bot :
+\n https://chat.whatsapp.com/Fb1V5mV2qIKILLPXC66clV
+\n*– Support Pairing Code*\n*– Disk Used Average 700mb*\n*_ Bot Size 3mb*\n*– Clean Code*\n*– No Encrypt*\n*– Support Newsletter*
+
+
+\n*– 乂 B E N E F I T*\n ◦  Free Update Always Time\n  ◦  Free Apikey Vip 30.000 Limit  \n  ◦  Free Fix\n  ◦  Free Hosting Bot
+
+\n*– 乂 C O N T A C T - P E R S O N*
+\n ◦   WhatsApp : https://wa.me/6285822146627`
 m.reply(esce)
 }
 handler.help = ['sc', 'sourcecode']
